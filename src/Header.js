0ac46b52d511dada7fb5f8271e72e5ac7dde4b96
imranjami <imranajami@gmail.com>
2021-03-08 15:26:36 -0800

    setup

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import logo from "./assets/logo.svg"
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className="header">
+      <p className="header__text">Create NFT</p>
+      <p>hello</p>
+    </div>
+  )
+}
+
+export default Header
