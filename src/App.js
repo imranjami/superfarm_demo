bcfc272df1250f50f8184691a582b88f9e48f94c
imranjami <imranajami@gmail.com>
2021-03-09 12:28:28 -0800

    favicon

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import "./App.css"
 import Footer from "./Footer"
 import Header from "./Header"
 import ProductCard from "./ProductCard"
+import { motion } from "framer-motion"
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="cards">
-        <ProductCard />
-        <ProductCard />
-      </div>
+      <motion.div
+        initial={{ y: "-100vh" }}
+        animate={{ y: "0vh" }}
+        transition={{ type: "ease-in", duration: 1 }}
+      >
+        <Header />
+      </motion.div>
 
-      <Footer />
+      <motion.div
+        className="cards"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 1 }}
+      >
+        <ProductCard />
+        <ProductCard />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.5, duration: 1 }}
+      >
+        <Footer />
+      </motion.div>
     </div>
   )
 }
